747c948290e8e8ff59efcbf9203a673f2f7213a7
Gonzalo Burgos <gburgostrincado@gmail.com>
2020-07-10 09:47:48 -0400

    Docs agregado

---
+++ docs/precache-manifest.d934a84786000dfc660f6adb5945a7fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "465c7bb7cb0ce463c3f88664eb0e4824",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c32039fdd93f97faea55",
+    "url": "/static/css/main.32e4dd79.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c32039fdd93f97faea55",
+    "url": "/static/js/main.8cf9e7ac.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
